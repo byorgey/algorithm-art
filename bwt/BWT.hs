c597bcee527b2b4985e3c8b260ffd754885e7c4d
Ryan Yates <ryates@cs.rochester.edu>
2013-10-17 17:20:26 -0400

    Trying different circles.

--- bwt/BWT.hs
+++ bwt/BWT.hs
@@ -48,7 +48,16 @@ unbwt t ys = take (length ys) (thread (spl ys t))
 type D = Diagram SVG R2
 
 alphabet :: Int -> D
-alphabet i = c # lc (acolor i) # lw 0.05
+alphabet i = c # lc (acolor i) # lw 0.05 # withEnvelope (circle 1 :: D)
+  where
+    l  = length cs
+    cs = [red, orange, yellow, green, blue, purple]
+
+    m  = abs i `mod` 2
+    c  = mconcat [circle (fromIntegral (m + 1 - r) / fromIntegral (m + 1) * (1 + 1/6 - (fromIntegral ((i `mod` 10) `div` 2) + 1) / 6)) | r <- [0..m]]
+
+alphabet' :: Int -> D
+alphabet' i = c # lc (acolor i) # lw 0.05
   where
     l  = length cs
     cs = [red, orange, yellow, green, blue, purple]
@@ -63,26 +72,36 @@ acolor i = cs !! (abs i `mod` l)
     cs = [red, orange, yellow, green, blue, purple]
 
 d :: D
-d = inputToBWT === strutY 1 === bwtToInput 
+d = hcat' def { sep = 1 } $ map centerXY 
+     [ inputToBWT
+     , bwtToRLE
+     , reflectX bwtToRLE
+     , bwtToInput
+     ]
   where
-    inputToBWT = hcat' def { sep = 0.1 } $ map centerXY
-      [ reflectX $ block rs  -- Rotations of s
+    vsep = 1 
+    hsep = 0.1
+  
+    inputToBWT = hcat' def { sep = hsep } $ map centerXY
+      [ block rs  -- Rotations of s
       , sorting head rs rs'
       , block rs' -- sorted rotations of s
       ]
 
-    bwtToInput = hcat' def { sep = 0.1 } $ map centerXY
-     [ reflectX $ block [[a,b] | (a,b) <- zip p [1..]]                      -- spl table
+    buildUnbwt = hcat' def { sep = hsep } $ map centerXY
+     [ reflectX $ block [[a,b] | (a,b) <- zip p [1..]]           -- spl table
      , sorting fst (zip p [1..]) (sortby (<=) (zip p [1..]))
      , block [[a,b,i] | (i,(a,b)) <- zip [1..] $ sortby (<=) (zip p [1..])] -- continued
-     , threads n p
      ]
+    bwtToInput = hcat' def { sep = hsep } $ map alignT [ buildUnbwt, threads n p ]
+
+    bwtToRLE = vcat' def { sep = vsep } $ map (hcat' def { sep = hsep} . map alphabet) $ groupBy (==) p
 
     threads n p =   alignL ((reflectY $ hcat $ take (length p * 2 - 1) ds))
-                === alignL (hcat' def { sep = 5+2.1 } (map (alphabet . snd) is))
+                === alignL (hcat' def { sep = 5+2+hsep } (map (alphabet . snd) is))
       where
         (is,ds) = mconcat [ ([(i,x)],
-                              [ moveTo (0 & (fromIntegral i * 2.1)) (centerXY (block [[x,j]]))
+                              [ moveTo (0 & (fromIntegral i * (2+vsep))) (centerXY (block [[x,j]]))
                               , connect i j
                               ])
                      | (i,x,j) <- ts
@@ -91,10 +110,10 @@ d = inputToBWT === strutY 1 === bwtToInput
         thread i (x,j) = (i,x,j) : thread j (spl j)
         spl t = fromJust $ lookup t (zip [1..] (sortby (<=) (zip p [1..])))
 
+    
 
-
-    row = hcat' def { sep = 0.1 }
-    block = vcat' def { sep = 0.1 } . map (row . map alphabet)
+    row = hcat' def { sep = hsep }
+    block = vcat' def { sep = vsep } . map (row . map alphabet)
 
     sorting f rs rs' = reflectY $ mconcat 
             [ connect i j # lc (acolor (f r))
@@ -104,7 +123,7 @@ d = inputToBWT === strutY 1 === bwtToInput
 --    connect i j = (0 & f i) ~~ (5 & f j) # lw 0.2
     connect i j = bez (0 & f i) (2 & f i) (3 & f j) (5 & f j) # lw 0.05
       where
-        f x = fromIntegral x * 2.1
+        f x = fromIntegral x * (2+vsep)
 
     rs  = rots s
     rs' = lexsort rs
@@ -116,7 +135,7 @@ d = inputToBWT === strutY 1 === bwtToInput
 bez a b c d = trailLike $ (fromSegments [bezier3 (b .-. a) (c .-. a) (d .-. a)]) `at` a
 
 d' :: D
-d' = hcat' def { sep = 0.1 } (map alphabet [0..10])
+d' = vcat' def { sep = 0.1 } (map alphabet [0..10])
 
 main = defaultMain (d # centerXY # pad 1.1)
 
