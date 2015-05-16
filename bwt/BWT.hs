a1d4fa0ef154a9cab13a014006dce65c5a5c2f36
Ryan Yates <ryates@cs.rochester.edu>
2015-05-16 13:03:15 -0400

    Last time we left our hero...

--- bwt/BWT.hs
+++ bwt/BWT.hs
@@ -14,6 +14,8 @@ import qualified Data.Map as M
 
 import Data.Default.Class
 
+import Debug.Trace
+
 
 -- Some parts from http://www.iis.sinica.edu.tw/~scm/pub/bwtJFP.pdf
 bwt ws = (bwn, bwp)
@@ -47,8 +49,8 @@ unbwt t ys = take (length ys) (thread (spl ys t))
 
 type D = Diagram SVG R2
 
-alphabet :: Int -> D
-alphabet i = c # lc (acolor i) # lw 0.05 # withEnvelope (circle 1 :: D)
+alphabet' :: Int -> D
+alphabet' i = c # lc (acolor i) # lw 0.05 # withEnvelope (circle 1 :: D)
   where
     l  = length cs
     cs = [red, orange, yellow, green, blue, purple]
@@ -56,8 +58,9 @@ alphabet i = c # lc (acolor i) # lw 0.05 # withEnvelope (circle 1 :: D)
     m  = abs i `mod` 2
     c  = mconcat [circle (fromIntegral (m + 1 - r) / fromIntegral (m + 1) * (1 + 1/6 - (fromIntegral ((i `mod` 10) `div` 2) + 1) / 6)) | r <- [0..m]]
 
-alphabet' :: Int -> D
-alphabet' i = c # lc (acolor i) # lw 0.05
+alphabet :: Int -> D
+alphabet (-1) = square 1 # lc red # lw 0.05 # withEnvelope (circle 1 :: D)
+alphabet i    = c # lc (acolor i) # lw 0.05
   where
     l  = length cs
     cs = [red, orange, yellow, green, blue, purple]
@@ -66,43 +69,67 @@ alphabet' i = c # lc (acolor i) # lw 0.05
     c  = mconcat [circle (fromIntegral (m + 1 - r) / fromIntegral (m + 1)) | r <- [0..m]]
 
 acolor :: Int -> Colour Double
+acolor (-1) = red
 acolor i = cs !! (abs i `mod` l)
   where
     l  = length cs
     cs = [red, orange, yellow, green, blue, purple]
 
 d :: D
-d = hcat' def { sep = 1 } $ map centerXY 
+d = squared -- horizontal 
+  where
+    vsep = 0.1 
+    hsep = 0.1
+
+    sweeping = (sweepLayout (Turn 1/2) $ map centerXY
+     [ block rs
+     , sorting 7 head rs rs'
+     , block rs'
+     , bwtToRLE
+     , reflectX bwtToRLE
+     , reflectX $ block [[a,b] | (a,b) <- zip p [1..]]
+     , sorting 7 fst (zip p [1..]) (sortby (<=) (zip p [1..]))
+     , block [[a,b,i] | (i,(a,b)) <- zip [1..] $ sortby (<=) (zip p [1..])]
+     ]) === centerXY (threads n p)
+ 
+    horizontal = hcat' def { sep = 1 } $ map centerXY 
      [ inputToBWT
      , bwtToRLE
      , reflectX bwtToRLE
      , bwtToInput
      ]
-  where
-    vsep = 1 
-    hsep = 0.1
-  
+
+    squared = vcat' def { sep = 2 } [ alignL top, translate (-2 & 0) $ alignL bottom ]
+      where
+        top    = reflectY $ f [ inputToBWT, bwtToRLE ]
+        bottom = rotate (Turn 1/2) $ reflectY $ f
+          [ reflectX bwtToRLE
+          , bwtToInput
+          ]
+        f ds = hcat' def { sep = 2 } (map centerXY $ ds)
+
     inputToBWT = hcat' def { sep = hsep } $ map centerXY
-      [ block rs  -- Rotations of s
-      , sorting head rs rs'
+      [ reflectX $ block rs  -- Rotations of s
+      , sorting 7 head rs rs'
       , block rs' -- sorted rotations of s
       ]
 
     buildUnbwt = hcat' def { sep = hsep } $ map centerXY
      [ reflectX $ block [[a,b] | (a,b) <- zip p [1..]]           -- spl table
-     , sorting fst (zip p [1..]) (sortby (<=) (zip p [1..]))
+     , sorting 23.1 fst (zip p [1..]) (sortby (<=) (zip p [1..]))
      , block [[a,b,i] | (i,(a,b)) <- zip [1..] $ sortby (<=) (zip p [1..])] -- continued
      ]
-    bwtToInput = hcat' def { sep = hsep } $ map alignT [ buildUnbwt, threads n p ]
+    bwtToInput = hcat' def { sep = 7 } $ map alignB [ buildUnbwt, threads n p ]
 
     bwtToRLE = vcat' def { sep = vsep } $ map (hcat' def { sep = hsep} . map alphabet) $ groupBy (==) p
 
-    threads n p =   alignL ((reflectY $ hcat $ take (length p * 2 - 1) ds))
-                === alignL (hcat' def { sep = 5+2+hsep } (map (alphabet . snd) is))
+    tw = -2
+    threads n p =   alignL (hcat' def { sep = tw+2+hsep } (map (alphabet . snd) is)) 
+                === alignL ((reflectY $ hcat' def { sep = tw } $ take (length p * 2 - 1) ds))
       where
         (is,ds) = mconcat [ ([(i,x)],
                               [ moveTo (0 & (fromIntegral i * (2+vsep))) (centerXY (block [[x,j]]))
-                              , connect i j
+--                              , connect tw i j
                               ])
                      | (i,x,j) <- ts
                      ]
@@ -115,22 +142,37 @@ d = hcat' def { sep = 1 } $ map centerXY
     row = hcat' def { sep = hsep }
     block = vcat' def { sep = vsep } . map (row . map alphabet)
 
-    sorting f rs rs' = reflectY $ mconcat 
-            [ connect i j # lc (acolor (f r))
+    sorting w f rs rs' = reflectY $ mconcat 
+            [ connect w i j # lc (acolor (f r))
             | (i,r) <- zip [0..] rs
             , let j = fromJust . findIndex (== r) $ rs'
             ]
 --    connect i j = (0 & f i) ~~ (5 & f j) # lw 0.2
-    connect i j = bez (0 & f i) (2 & f i) (3 & f j) (5 & f j) # lw 0.05
+    connect w i j = bez (0 & f i) (w*2/5 & f i) (w*3/5 & f j) (w & f j) # lw 0.05
       where
         f x = fromIntegral x * (2+vsep)
 
     rs  = rots s
     rs' = lexsort rs
 
-    s = map ((subtract (ord '0')) . ord) "101103107109113" -- This should be something more meaningful
+    s = (-1) : map ((subtract (ord '0')) . ord) "101103107109113" -- This should be something more meaningful
     (n,p) = bwt s
 
+
+sweepLayout sweep ds = mconcat $ zipWith f ds (scanl (+) 0 ts)
+  where
+    f d t = d # rotate (Turn 1/4) # alignL # moveTo (r & 0) # rotate t
+
+    r = total / (tau * getTurn sweep)
+    l = fromIntegral $ length ds
+    bbs = map boundingBox ds
+    ws' = map (fst . unr2 . boxExtents) bbs
+    wh = head ws'
+    wl = last ws'
+    ws = (wh / 2) : ((zipWith ave<*>tail) (drop 1 . take (length ds - 2) $ ws')) ++ [wl / 2]
+    ave a b = (a + b) / 2
+    total = sum ws
+    ts = map ((/(Turn total)) . (*sweep) . Turn) ws
 
 bez a b c d = trailLike $ (fromSegments [bezier3 (b .-. a) (c .-. a) (d .-. a)]) `at` a
 
@@ -138,6 +180,7 @@ d' :: D
 d' = vcat' def { sep = 0.1 } (map alphabet [0..10])
 
 main = defaultMain (d # centerXY # pad 1.1)
+-- main = defaultMain (sweepLayout (Turn 1/2) (map square [1,2,4,3,2,3,2,1,2]) # centerXY # pad 1.1)
 
 -----------------------------------
 
